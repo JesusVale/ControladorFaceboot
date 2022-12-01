7821a1dc4432b5f547ef2ba9fd8f6054159dbdc5
MarcoIrineo <marco.irineo228880@potros.itson.edu.mx>
2022-12-01 00:26:34 -0700

    Excepciones hashtag
    2

--- Controlador_Faceboot/src/main/java/coninterfaces/IFachadaModeloHashtag.java
+++ Controlador_Faceboot/src/main/java/coninterfaces/IFachadaModeloHashtag.java
@@ -6,6 +6,7 @@ package coninterfaces;
 
 import entidades.Hashtag;
 import entidades.Publicacion;
+import excepciones.PersistException;
 import java.util.List;
 
 /**
@@ -13,9 +14,9 @@ import java.util.List;
  * @author tonyd
  */
 public interface IFachadaModeloHashtag {
-    public Hashtag agregarHashtag(Hashtag hashtag);
-    public List agregarHashtags(List<Hashtag> hashtags);
-    public Hashtag consultarHashtag(Integer idHashtag);
-    public Hashtag eliminarHashtag(Integer idHashtag);
+    public Hashtag agregarHashtag(Hashtag hashtag) throws PersistException;
+    public List<Hashtag> agregarHashtags(List<Hashtag> hashtags) throws PersistException;
+    public Hashtag consultarHashtag(Integer idHashtag) throws PersistException;
+    public Hashtag eliminarHashtag(Integer idHashtag) throws PersistException;
     public Hashtag consultarPorTema(String tema) throws Exception;
 }
--- Controlador_Faceboot/src/main/java/controladores/ControladorHashtag.java
+++ Controlador_Faceboot/src/main/java/controladores/ControladorHashtag.java
@@ -8,8 +8,10 @@ import commodelo.FachadaModeloHashtag;
 import coninterfaces.IFachadaModeloHashtag;
 import entidades.Hashtag;
 import eventos.Eventos;
+import excepciones.PersistException;
 import java.util.List;
 import peticiones.PeticionHashtag;
+import peticiones.PeticionHashtags;
 
 /**
  *
@@ -22,23 +24,42 @@ public class ControladorHashtag {
     public ControladorHashtag() {
         this.fachadaHashtag = new FachadaModeloHashtag();
     }
-
-    public Hashtag registrarHashtag(Hashtag hashtag) {
-        return fachadaHashtag.agregarHashtag(hashtag);
+    public PeticionHashtag registrarHashtag(Hashtag hashtag){
+        try{
+            Hashtag hashtagRegistrado = fachadaHashtag.agregarHashtag(hashtag);
+            return new PeticionHashtag(Eventos.registrarHashtag, 200, hashtagRegistrado);
+        }catch(PersistException pe){
+            return new PeticionHashtag(Eventos.consultarHashtag, 503, pe.getMessage());
+        }
     }
 
-    public List registrarHashtags(List<Hashtag> hashtags) {
-        return fachadaHashtag.agregarHashtags(hashtags);
+    public PeticionHashtags registrarHashtags(List<Hashtag> hashtags){
+        try{
+            List<Hashtag> hashtagsRegistrados = fachadaHashtag.agregarHashtags(hashtags);
+            return new PeticionHashtags(Eventos.registrarHashtags, 200, hashtagsRegistrados);
+        }catch(PersistException pe){
+            return new PeticionHashtags(Eventos.registrarHashtags, 503, pe.getMessage());
+        }
     }
 
-    public Hashtag consultarHashtag(Integer idHashtag) {
-        return fachadaHashtag.consultarHashtag(idHashtag);
-    }
-
-    public Hashtag registrarHashtag(Integer idHashtag) {
-        return fachadaHashtag.eliminarHashtag(idHashtag);
+    public PeticionHashtag consultarHashtag(Integer idHashtag){
+        try{
+            Hashtag hashtagConsultado = fachadaHashtag.consultarHashtag(idHashtag);
+            return new PeticionHashtag(Eventos.consultarHashtag, 200, hashtagConsultado);
+        }catch(PersistException pe){
+            return new PeticionHashtag(Eventos.consultarHashtag, 503, pe.getMessage());
+        }
     }
     
+    public PeticionHashtag eliminarHashtag(Integer idHashtag){
+        try{
+            Hashtag hashtagEliminado = fachadaHashtag.eliminarHashtag(idHashtag);
+            return new PeticionHashtag(Eventos.eliminarHashtag, 200, hashtagEliminado);
+        }catch(PersistException pe){
+            return new PeticionHashtag(Eventos.eliminarHashtag, 503, pe.getMessage());
+        }
+    }
+ 
     public PeticionHashtag consultarPorNombre(String tema){
         try{
             Hashtag hashtagEncontrado = fachadaHashtag.consultarPorTema(tema);
